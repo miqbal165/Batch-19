d690a9b3fe4bc041d6e1a8aef36c521b631a0cc1
= <=>
2026-06-04 11:09:53 +0700

    Finished Logic Exercise

---
+++ LogicExercise/Program.cs
@@ -0,0 +1,17 @@
+﻿for (int i = 1; i < 100; i++)
+{
+    if (i % 3 == 0)
+    {
+        Console.WriteLine("foo");
+    } else if (i % 5 == 0)
+    {
+        Console.WriteLine("bar");
+    } else if (i % 3 == 0 && i % 5 == 0)
+    {
+        Console.WriteLine("foobar");
+        continue;
+    } else
+    {
+        Console.WriteLine(i);
+    }
+}
